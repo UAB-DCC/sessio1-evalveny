c2ef75bb3479053b7798ab86b01330264ef04606
GemmaSan <36231120+GemmaSan@users.noreply.github.com>
2018-04-16 15:18:12 +0200

    ex7.3 novaversio
    ex7.3 nova versio

--- Problemes/Tema_2/Ex7_3/Ex7_3/main.cpp
+++ Problemes/Tema_2/Ex7_3/Ex7_3/main.cpp
@@ -9,33 +9,35 @@ void intercanvia(int* p_x, int* p_y)
 	*p_x = *p_y;
 	*p_y = tmp;
 }
-void intercanviaMajorMenor(int v[], int mida)
+void intercanviaMajorMenor(int v[], int mida,int* min, int* max)
 {
-	int min = 0;
-	int max = 0;
+	*min = 0;
+	*max = 0;
 	for (int i = 1; i < mida; i++)
 	{
-		if (v[min] < v[i])
+		if (v[*min] < v[i])
 		{
-			min = i;
+			*min = i;
 		}
-		if (v[max] > v[i])
+		if (v[*max] > v[i])
 		{
-			max = i;
+			*max = i;
 		}
 	}
-	intercanvia(&v[min], &v[max]);
+	intercanvia(&v[*min], &v[*max]);
 }
 
 const int MAX = 10;
 int main()
 {
 	int v[MAX] = {7,8,2,4,6,1,9,10,13,11};
-	intercanviaMajorMenor(v, MAX);
+	int min, max;
+	intercanviaMajorMenor(v, MAX,min,max);
 	for (int i = 0; i < MAX; i++)
 	{
 		cout << v[i] <<endl;
 	}
+	cout << "Min: " << min << " ; Max: " << max;
 
 	return 0;
 }
